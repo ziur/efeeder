73764704e29358235632e8c8367f186387246c5c
Alejandro Ruiz <ruiz.ac@gmail.com>
2016-09-08 16:02:47 -0400

    Added support to actions based on commands. The commands are registered with the Command annotation

---
+++ src/main/java/org/jala/efeeder/voting/VotingCommand.java
@@ -0,0 +1,34 @@
+package org.jala.efeeder.voting;
+
+import org.jala.efeeder.api.command.Command;
+import org.jala.efeeder.api.command.CommandUnit;
+import org.jala.efeeder.api.command.In;
+import org.jala.efeeder.api.command.Out;
+import org.jala.efeeder.api.command.impl.DefaultOut;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alejandro on 08-09-16.
+ */
+@Command
+public class VotingCommand implements CommandUnit {
+    @Override
+    public Out execute(In context) throws Exception {
+        Connection connection = context.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("Select * from items");
+        Out out = new DefaultOut();
+        List<Vote> votes = new ArrayList<Vote>();
+        while (resultSet.next()) {
+            votes.add(new Vote(resultSet.getString(1), resultSet.getString(2)));
+        }
+        out.addResult("votes", votes);
+        out.forward("voting/vote.jsp");
+        return out;
+    }
+}
