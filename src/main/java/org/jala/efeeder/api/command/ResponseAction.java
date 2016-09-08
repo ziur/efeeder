6aaad7a5840a8ec2c4ec30f7882811ed53e45369
Alejandro Ruiz <ruiz.ac@gmail.com>
2016-09-08 12:02:29 -0400

    Added basic page template and forward/redirect mechanism

---
+++ src/main/java/org/jala/efeeder/api/command/ResponseAction.java
@@ -0,0 +1,33 @@
+package org.jala.efeeder.api.command;
+
+/**
+ * Created by alejandro on 08-09-16.
+ */
+public class ResponseAction {
+    private String url;
+    private boolean isRedirect;
+
+    public ResponseAction() {
+        isRedirect = false;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getFordwarUrl() {
+        return "/WEB-INF/" + url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isRedirect() {
+        return isRedirect;
+    }
+
+    public void setRedirect(boolean redirect) {
+        isRedirect = redirect;
+    }
+}
--- src/main/java/org/jala/efeeder/api/command/impl/DefaultOut.java
+++ src/main/java/org/jala/efeeder/api/command/impl/DefaultOut.java
@@ -3,20 +3,23 @@ package org.jala.efeeder.api.command.impl;
 import org.jala.efeeder.api.command.ExitStatus;
 import org.jala.efeeder.api.command.MessageType;
 import org.jala.efeeder.api.command.Out;
+import org.jala.efeeder.api.command.ResponseAction;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by alejandro on 07-09-16.
  */
-public class DefaultOut extends HashMap implements Out {
+public class DefaultOut implements Out {
 
     private List<String> errors;
+    private ResponseAction responseAction;
+    private Map<String, Object> context;
 
     public DefaultOut() {
+        context = new HashMap<>();
         errors = new ArrayList<>();
+        responseAction = new ResponseAction();
     }
 
     @Override
@@ -36,7 +39,7 @@ public class DefaultOut extends HashMap implements Out {
 
     @Override
     public void addResult(String key, Object value) {
-
+        context.put(key, value);
     }
 
     @Override
@@ -44,14 +47,31 @@ public class DefaultOut extends HashMap implements Out {
         return null;
     }
 
+
     @Override
-    public List<Object> getResults(MessageType type) {
-        return null;
+    public Set<Map.Entry<String, Object>> getResults() {
+        return context.entrySet();
     }
 
     @Override
     public void addError(Throwable e) {
         errors.add(e.getMessage());
+    }
+
+    @Override
+    public ResponseAction getResponseAction() {
+        return responseAction;
+    }
+
+    public Out redirect(String url) {
+        responseAction.setRedirect(true);
+        responseAction.setUrl(url);
+        return this;
+    }
+
+    public Out forward(String page) {
+        responseAction.setUrl(page);
+        return this;
     }
 
     @Override
