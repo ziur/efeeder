f5a96ccfcb582567aadd926fad9d0af5617695ed
0x3 <0x3@github.com>
2016-09-12 15:40:58 -0400

    Changes to create the json data with java code instead javascript.

--- src/main/java/org/jala/efeeder/wheel/WheeldecideCommand.java
+++ src/main/java/org/jala/efeeder/wheel/WheeldecideCommand.java
@@ -8,16 +8,14 @@ package org.jala.efeeder.wheel;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import org.jala.efeeder.api.command.Command;
 import org.jala.efeeder.api.command.CommandUnit;
 import org.jala.efeeder.api.command.In;
 import org.jala.efeeder.api.command.Out;
 import org.jala.efeeder.api.command.impl.DefaultOut;
 
-/**votes
- *
+
+/**
  * @author alexander_castro
  */
 @Command
@@ -25,25 +23,61 @@ public class WheeldecideCommand implements CommandUnit{
 
     @Override
     public Out execute(In context) throws Exception {
-//        String a = context.getParameter("a") + "- Param";
         Connection connection = context.getConnection();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("Select * from User");
-        Out out = new DefaultOut();
-        List<User> userList = new ArrayList<User>();
+        
+        int count = 0;
+        StringBuilder jsonData = new StringBuilder(0xFF);
+        jsonData.append("{\"items\":[");
         while (resultSet.next()) {
-            userList.add(new User(resultSet.getString(1), resultSet.getString(2)));
+            if (count > 0) jsonData.append(",");
+            jsonData.append(escapeJsonString(resultSet.getString(2)));
+            ++count;
         }
-        out.addResult("users1", userList);
-        out.addResult("chosen", getRandomIndexPerson(userList.size()));
+	jsonData.append("],'chosen':");
+        jsonData.append(getRandomIndexPerson(count));
+        jsonData.append("}");
+        
+	
+	Out out = new DefaultOut();
+        out.addResult("jsonData", jsonData.toString());
         out.forward("wheeldecide/wheel.jsp");
         return out;
     }
     
-    private int getRandomIndexPerson(int numberOfPersons){
-        double random = (Math.random() * 10) + 1;
-        int d = (int)((random * numberOfPersons) / 10);
-        return d;
+    private static int getRandomIndexPerson(int numberOfPersons){
+        return (int)Math.floor(Math.random() * numberOfPersons);
     }
+    
+    private static String escapeJsonString(String string) {
+        if (null == string || 0 == string.length()) {
+            return "\"\"";
+        }
+
+        final int len = string.length();
+        StringBuilder sb = new StringBuilder(len + 8);
+        sb.append('"');
+
+        final String escapeInput = "\\\"/\b\t\n\f\r";
+        final String escapeOutput = "\\\"/btnfr";
+        for (int i = 0; i < len; ++i) {
+            final char c = string.charAt(i);
+            final int index = escapeInput.indexOf(c);
+            if (index != -1)
+            {
+                sb.append('\\');
+                sb.append(escapeOutput.charAt(index));
+            }
+            else if (c < ' ') {
+                sb.append(String.format("\\u%04x", c));
+            } else {
+                sb.append(c);
+            }
+        }
+        sb.append('"');
+        return sb.toString();
+    }
+
     
 }
