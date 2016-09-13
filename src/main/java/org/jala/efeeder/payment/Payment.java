cf842b3a389dd927df6b638f5e6796dce2a832b7
Roger Ayaviri <Roger Ayaviri@RAYAVIRIC-DH01.jalasoft.local>
2016-09-13 09:24:20 -0400

    update PaymentCommand and create orders table

--- src/main/java/org/jala/efeeder/payment/Payment.java
+++ src/main/java/org/jala/efeeder/payment/Payment.java
@@ -17,8 +17,8 @@ public class Payment {
         this.order = order;
         this.cost = cost;
     }
-        
-    public String getId() {
+
+	public String getId() {
         return id;
     }
 
--- src/main/java/org/jala/efeeder/payment/PaymentCommand.java
+++ src/main/java/org/jala/efeeder/payment/PaymentCommand.java
@@ -1,5 +1,8 @@
 package org.jala.efeeder.payment;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import org.jala.efeeder.api.command.Command;
@@ -17,19 +20,23 @@ public class PaymentCommand implements CommandUnit {
 
     @Override
     public Out execute(In parameters) throws Exception {
+
         List<Payment> payments = new ArrayList<>();
-        
-        String meetingName = "Chicken Kingdom";
-        payments.add(new Payment("1", "Roger Ayaviri", "Pierna de poyo", 10.0));
-        payments.add(new Payment("1", "Mirko Terrazas", "Hamburguesa", 0.0));
-        payments.add(new Payment("1", "Ricardo Ruiz", "Papas y refresco", 10.0));
-        payments.add(new Payment("1", "Rodrigo Ruiz", "Ensalada", 15.0));
-        payments.add(new Payment("1", "Alejandro Ruiz", "Todo lo anterior", 5.0));
+
         Out out = new DefaultOut();
-        
-        out.addResult("meetingName", meetingName);
+        Connection connection = parameters.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("Select o.id_food_meeting, u.name, o.order_name, o.cost from orders o, users u where o.id_food_meeting = ?");
+        preparedStatement.setInt(1, Integer.valueOf(parameters.getParameter("id_food_meeting")));
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            payments.add(new Payment(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getDouble(4)));
+        }
+
+        out.addResult("meetingName", parameters.getParameter("meeting_name"));
         out.addResult("payments", payments);
         out.forward("payment/payment.jsp");
+
         return out;
-    }    
+    }
 }
