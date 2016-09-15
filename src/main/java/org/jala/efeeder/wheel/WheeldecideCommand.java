76badfaa4b8ad0e485753779aa3cd9e0e656f416
0x3 <0x3@github.com>
2016-09-15 17:53:44 -0400

    Fix table name typo

--- src/main/java/org/jala/efeeder/wheel/WheeldecideCommand.java
+++ src/main/java/org/jala/efeeder/wheel/WheeldecideCommand.java
@@ -7,6 +7,7 @@ package org.jala.efeeder.wheel;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.jala.efeeder.api.command.Command;
 import org.jala.efeeder.api.command.CommandUnit;
 import org.jala.efeeder.api.command.In;
@@ -23,24 +24,58 @@ public class WheeldecideCommand implements CommandUnit{
     @Override
     public Out execute(In context) throws Exception {       
         PreparedStatement pStatement = context.getConnection()
-                .prepareStatement("select name from users,orders where id_food_meeting=? and id=id_user");
+                .prepareStatement("select name from user,orders where id_food_meeting=? and id=id_user");
         
-        pStatement.setInt(1, Integer.valueOf(context.getParameter("id_food_meeting2")));
+        String message = "";
         
-        ResultSet resultSet = pStatement.executeQuery();
+        String parameter = context.getParameter("id_food_meeting2");
+        
+        message = "Parameter is: " + parameter;
+        
+        try
+        {
+            pStatement.setInt(1, Integer.valueOf(parameter));
+        }
+        catch (NumberFormatException e)
+        {
+            message = "Not a number: " + parameter;
+        }
+        
+        ResultSet resultSet;
+        
+        try
+        {
+            resultSet = pStatement.executeQuery();
+        }
+        catch (SQLException e)
+        {
+            resultSet = null;
+            message += ", query failed!";
+        }
                 
         int count = 0;
         StringBuilder jsonData = new StringBuilder(0xFF);
-        jsonData.append("'{\"items\":[");
-        while (resultSet.next()) {
-            if (count > 0) jsonData.append(",");
-            jsonData.append(escapeJsonString(resultSet.getString(1)));
-            ++count;
-        }
-	jsonData.append("],\"chosen\":");
-        jsonData.append(getRandomIndexPerson(count));
-        jsonData.append("}'");
         
+        
+        if (resultSet != null)
+        {
+            jsonData.append("'{\"items\":[");
+            while (resultSet.next()) {
+                if (count > 0) jsonData.append(",");
+                jsonData.append(escapeJsonString(resultSet.getString(1)));
+                ++count;
+            }
+            jsonData.append("],\"chosen\":");
+            jsonData.append(getRandomIndexPerson(count));
+            jsonData.append("}'");
+            
+        }
+        else
+        {
+            jsonData.append("'{\"items\":[");
+            jsonData.append(escapeJsonString(message));
+            jsonData.append("],\"chosen\":0}'");
+        }
 	
 	Out out = new DefaultOut();
         out.addResult("jsonData", jsonData.toString());
