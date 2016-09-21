b63dfe0e609d096e7ef946ff2ef6e8f5cdec03eb
Mirko Jhovanny Terrazas Vargas <mterrazas@pros.com>
2016-09-21 16:23:47 -0400

    Added "go to settings button"

--- src/main/webapp/assets/js/foodMeetings.js
+++ src/main/webapp/assets/js/foodMeetings.js
@@ -3,6 +3,10 @@ $('.food-meetings').masonry({
     columnWidth: 50
 });
 
-$(".meeting-date").each(function(value){                           
-    $(this).text(moment($(this).data("date"), "YYYY-MM-DD").calendar());
+$(".quick-view-date").each(function(){                           
+    $(this).text(moment($(this).closest(".meeting").data("date"), "YYYY-MM-DD").calendar());
+});
+
+$(".detailed-view-date").each(function(){                           
+    $(this).text("Eat time : " + moment($(this).closest(".meeting").data("date"), "YYYY-MM-DD").format('MMMM Do YYYY, h:mm:ss a'));
 });
