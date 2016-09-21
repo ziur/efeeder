e7b364618a8a6d1d03ca9ec276aa51e9513249f3
Roger Ayaviri <Roger Ayaviri@RAYAVIRIC-DH01.jalasoft.local>
2016-09-21 16:50:09 -0400

    add create meeting

--- src/main/java/org/jala/efeeder/foodmeeting/CreateFoodMeetingCommand.java
+++ src/main/java/org/jala/efeeder/foodmeeting/CreateFoodMeetingCommand.java
@@ -6,7 +6,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
-import java.util.List;
 
 import org.jala.efeeder.api.command.Command;
 import org.jala.efeeder.api.command.CommandUnit;
@@ -18,7 +17,6 @@ import org.jala.efeeder.api.utils.JsonConverter;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-
 
 /**
  * Created by alejandro on 09-09-16.
@@ -36,15 +34,12 @@ public class CreateFoodMeetingCommand implements CommandUnit {
         if (parameters.getParameter("meeting_name") == null && parameters.getParameter("eventdate") == null) {
             return out.forward("foodmeeting/foodMeeting.jsp");
         }
-        List<String> friends = parameters.getParameters("friends");
-
 
         PreparedStatement stm = parameters.getConnection()
                                         .prepareStatement(INSERT_FOOD_MEETING_SQL, RETURN_GENERATED_KEYS);
 
         DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
         DateTime dateTime = formatter.parseDateTime(parameters.getParameter("eventdate"));
-        
 
         Timestamp eventDate = new Timestamp(dateTime.getMillis());
         Timestamp createdAt = new Timestamp(System.currentTimeMillis());
@@ -53,7 +48,6 @@ public class CreateFoodMeetingCommand implements CommandUnit {
         stm.setString(2, "https://images.sciencedaily.com/2016/06/160614100258_1_540x360.jpg");
         stm.setTimestamp(3, eventDate);
         stm.setTimestamp(4, createdAt);
-        
 
         stm.executeUpdate();
 
@@ -62,9 +56,9 @@ public class CreateFoodMeetingCommand implements CommandUnit {
         int meetingId = generatedKeysResultSet.getInt(1);
         stm.close();
 
-        FoodMeeting foodMeeting = new FoodMeeting(meetingId, parameters.getParameter("meeting_name"), "",
+        FoodMeeting foodMeeting = new FoodMeeting(meetingId, parameters.getParameter("meeting_name"),
+                "https://images.sciencedaily.com/2016/06/160614100258_1_540x360.jpg",
                 new Date(eventDate.getTime()), new Date(createdAt.getTime()));
-
 
         return OutBuilder.response("application/json", JsonConverter.objectToJSON(foodMeeting));
     }
