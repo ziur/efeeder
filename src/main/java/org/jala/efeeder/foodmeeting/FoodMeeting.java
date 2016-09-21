174f970dfc9103b557a9cd4d896a16ecef6c8fdf
Roger Ayaviri <Roger Ayaviri@RAYAVIRIC-DH01.jalasoft.local>
2016-09-21 09:52:25 -0400

    Add food meetings

--- src/main/java/org/jala/efeeder/foodmeeting/FoodMeeting.java
+++ src/main/java/org/jala/efeeder/foodmeeting/FoodMeeting.java
@@ -2,44 +2,25 @@ package org.jala.efeeder.foodmeeting;
 
 import java.sql.Date;
 
+import lombok.Data;
+
 /**
  * Created by alejandro on 09-09-16.
  */
+@Data
 public class FoodMeeting {
     private int id;
     private String name;
+    private String imageLink;
     private Date createdAt;
 
     public FoodMeeting() {
     }
 
-    public FoodMeeting(int id, String name, Date createdAt) {
+    public FoodMeeting(int id, String name,String imageLink, Date createdAt) {
         this.id = id;
         this.name = name;
         this.createdAt = createdAt;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
+        this.imageLink = imageLink;
     }
 }
--- src/main/java/org/jala/efeeder/foodmeeting/FoodMeetingCommand.java
+++ src/main/java/org/jala/efeeder/foodmeeting/FoodMeetingCommand.java
@@ -1,16 +1,16 @@
 package org.jala.efeeder.foodmeeting;
 
-import org.jala.efeeder.api.command.Command;
-import org.jala.efeeder.api.command.CommandUnit;
-import org.jala.efeeder.api.command.In;
-import org.jala.efeeder.api.command.Out;
-import org.jala.efeeder.api.command.impl.DefaultOut;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jala.efeeder.api.command.Command;
+import org.jala.efeeder.api.command.CommandUnit;
+import org.jala.efeeder.api.command.In;
+import org.jala.efeeder.api.command.Out;
+import org.jala.efeeder.api.command.impl.DefaultOut;
 
 /**
  * Created by alejandro on 09-09-16.
@@ -22,10 +22,12 @@ public class FoodMeetingCommand implements CommandUnit {
         Out out = new DefaultOut();
         Connection connection = parameters.getConnection();
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("Select * from food_meeting order by created_at");
+        ResultSet resultSet = statement
+                .executeQuery("Select id, name, image_link, created_at from food_meeting order by created_at");
         List<FoodMeeting> foodMeetings = new ArrayList<>();
         while (resultSet.next()) {
-            foodMeetings.add(new FoodMeeting(resultSet.getInt(1), resultSet.getString(2), resultSet.getDate(3)));
+            foodMeetings.add(new FoodMeeting(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3),
+                    resultSet.getDate(4)));
         }
         out.addResult("foodMeetings", foodMeetings);
         return out.forward("foodmeeting/foodMeeting.jsp");
