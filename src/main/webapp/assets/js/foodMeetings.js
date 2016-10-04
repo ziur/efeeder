a7e2c2d38a681ea80952e9f29365d80530131640
Danitza Machicado <danitza.machicado@jalasoft.com>
2016-10-04 16:39:06 -0400

    Redirect to pages according status

--- src/main/webapp/assets/js/foodMeetings.js
+++ src/main/webapp/assets/js/foodMeetings.js
@@ -1,117 +1,131 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$('.collapsible').collapsible({
-		accordion : false
-	});
+    $('.collapsible').collapsible({
+        accordion: false
+    });
 
-	$('.datepicker').pickadate({
-		selectMonths: true,
-		selectYears: 15
-	});
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYears: 15
+    });
 
-	$('#time').pickatime({
-		autoclose: false,
-		twelvehour: false,
-		autoclose: true,
-		vibrate: true
-	});
+    $('#time').pickatime({
+        autoclose: false,
+        twelvehour: false,
+        autoclose: true,
+                vibrate: true
+    });
 
-	$(".meeting-img").click(function () {
-		var page = $(this).data("meetingStatus") === "Voting" ? "suggestions" : "suggestions";
-		window.location.href = '/action/'+page+'?id_food_meeting=' +
-		$(this).data("meetingId");
-	});
+    $(".meeting-img").click(function() {
+        var page = "suggestions";
+        var meetingStatus = $(this).data("meetingStatus");
 
-	var foodMeetings = $('.food-meetings');
+        switch (meetingStatus) {
+            case 'Voting':
+                page = "suggestions";
+                break;
+            case 'Order':
+                page = "order";
+                break;
+            case 'Raffle':
+                page = "raffle";
+                break;
+            case 'Finish':
+                page = "finish";
+                break;
+        }
+        window.location.href = '/action/' + page + '?id_food_meeting=' +
+                $(this).data("meetingId");
+    });
 
-	foodMeetings.imagesLoaded()
-		.done(function(){
-			foodMeetings.masonry({
-				itemSelector: '.grid-item',
-				columnWidth: 50
-			});
-		});
+    var foodMeetings = $('.food-meetings');
 
-	$(".quick-view-date").each(function(){
-		$(this).text(moment($(this).closest(".meeting").data("date"), "YYYY-MM-DD hh:mm:ss.s").calendar());
-	});
+    foodMeetings.imagesLoaded()
+            .done(function() {
+                foodMeetings.masonry({
+                    itemSelector: '.grid-item',
+                    columnWidth: 50
+                });
+            });
 
-	$(".detailed-view-date").each(function(){  
-		$(this).text("Eat time : " + moment($(this).closest(".meeting").data("date"), "YYYY-MM-DD hh:mm:ss.s").format('MMMM Do YYYY, h:mm a'));
-	});
+    $(".quick-view-date").each(function() {
+        $(this).text(moment($(this).closest(".meeting").data("date"), "YYYY-MM-DD hh:mm:ss.s").calendar());
+    });
 
-	$("#addMeeting").validate({
+    $(".detailed-view-date").each(function() {
+        $(this).text("Eat time : " + moment($(this).closest(".meeting").data("date"), "YYYY-MM-DD hh:mm:ss.s").format('MMMM Do YYYY, h:mm a'));
+    });
 
-		errorPlacement: function(error, element) {
-			var placement = $(element).data('error');
-			if (placement) {
-				$(placement).append(error)
-			} else {
-				$(error).addClass("red-text");
-				$(element).addClass("invalid ");
-				error.insertAfter(element);
-			}
-		},
-		errorElement: "div",
-		rules : {
-			meeting_name: "required",
-			time: "required",
-			date: {
-				required: true,
-				date: true
-			  }
-		},
+    $("#addMeeting").validate({
+        errorPlacement: function(error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error)
+            } else {
+                $(error).addClass("red-text");
+                $(element).addClass("invalid ");
+                error.insertAfter(element);
+            }
+        },
+        errorElement: "div",
+        rules: {
+            meeting_name: "required",
+            time: "required",
+            date: {
+                required: true,
+                date: true
+            }
+        },
+        messages: {
+            meeting_name: "Please enter your meeting name",
+            date: "Please enter a date",
+            time: "Please enter a time",
+        }
+    });
 
-		messages: {
-			meeting_name: "Please enter your meeting name",
-			date: "Please enter a date",
-			time: "Please enter a time",
-		}
-	});
+    $("#addMeeting").submit(function(event) {
 
-	$("#addMeeting").submit(function (event) {
+        event.preventDefault();
 
-		event.preventDefault();
-		
-		if($("#addMeeting").valid())
-		{
-		
-			var $form = $( this ),
-			meeting_name = $form.find( "input[name='meeting_name']" ).val(),
-			date = $form.find( "input[name='date']" ).val(),
-			time = $form.find( "input[name='time']" ).val(),
-			url = $form.attr( "action" );
+        if ($("#addMeeting").valid())
+        {
 
-			var imageLink = $("#ImageLinkId").val();
+            var $form = $(this),
+                    meeting_name = $form.find("input[name='meeting_name']").val(),
+                    date = $form.find("input[name='date']").val(),
+                    time = $form.find("input[name='time']").val(),
+                    url = $form.attr("action");
 
-			// Send the data using post
-			var posting = $.post( url, { meeting_name: meeting_name, image_link: imageLink,  eventdate:moment(date+" "+time,"DD MMMM, YYYY hh:mm").format("DD/MM/YYYY HH:mm:ss")} );
+            var imageLink = $("#ImageLinkId").val();
 
-			// Put the results in a div
-			posting.done(function( data ) {
-				location.reload(); 
-			});
-		}
-	});
+            // Send the data using post
+            var posting = $.post(url, {meeting_name: meeting_name, image_link: imageLink, eventdate: moment(date + " " + time, "DD MMMM, YYYY hh:mm").format("DD/MM/YYYY HH:mm:ss")});
 
-	$("#ImageLinkId").change(function (event) {
+            // Put the results in a div
+            posting.done(function(data) {
+                location.reload();
+            });
+        }
+    });
 
-		var imageLink = $("#ImageLinkId").val();
+    $("#ImageLinkId").change(function(event) {
 
-		$("#ImageCard").attr("src",imageLink);
-	});
+        var imageLink = $("#ImageLinkId").val();
 
-	$("#AddNewMeeting").click(function (event) {
-		
-		$("#NewMeetingCard").show();
-		foodMeetings.masonry({
-			itemSelector: '.grid-item',
-			columnWidth: 50
-		});
-	});
+        $("#ImageCard").attr("src", imageLink);
+    });
 
-	$("#cancelCreateMeeting").click(function (event) {
-		location.reload(); 
-	});
+    $("#AddNewMeeting").click(function(event) {
+
+        $("#NewMeetingCard").show();
+        foodMeetings.masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 50
+        });
+    });
+
+    $("#cancelCreateMeeting").click(function(event) {
+        location.reload();
+    });
 
 });
