7ed108f356215eefba0b7ca4b41966233e8939b2
Denis Vasquez <denis.vasquez@jalasoft.com>
2016-10-31 20:59:21 -0400

    commands added

---
+++ src/main/java/org/jala/efeeder/user/UserMeetingsCommand.java
@@ -0,0 +1,22 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jala.efeeder.user;
+
+import org.jala.efeeder.api.command.Command;
+import org.jala.efeeder.api.command.CommandUnit;
+import org.jala.efeeder.api.command.In;
+import org.jala.efeeder.api.command.Out;
+import org.jala.efeeder.api.command.impl.DefaultOut;
+
+@Command
+public class UserMeetingsCommand implements CommandUnit{
+    @Override
+    public Out execute(In parameters) throws Exception {
+        Out out = new DefaultOut();
+        return out.forward("user/history.jsp");
+    }
+}
