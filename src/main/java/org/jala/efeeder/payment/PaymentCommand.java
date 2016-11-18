6d51c06bb9f5b9d47c7b28f775f3e7aeba2b1329
Rodrigo Ruiz <rodrigoruizrdg@gmail.com>
2016-11-18 11:46:24 -0400

    Merge comments

--- src/main/java/org/jala/efeeder/payment/PaymentCommand.java
+++ src/main/java/org/jala/efeeder/payment/PaymentCommand.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jala.efeeder.payment;
 
 import java.sql.Connection;
@@ -33,98 +28,98 @@ import org.jala.efeeder.user.User;
 @Command
 public class PaymentCommand implements CommandUnit {
 
-    @Override
-    public Out execute(In parameters) throws Exception {
-        Out out = new DefaultOut();
-        Connection connection = parameters.getConnection();
-        BuyerManager buyerManager = new BuyerManager(connection);
-        String idFoodMeeting = parameters.getParameter("id_food_meeting");
-        int idUser = parameters.getUser().getId();
-        List<Order> orders = getOrders(connection, idFoodMeeting);
-        Buyer buyer = buyerManager.getBuyerByFoodMeetingId(Integer.valueOf(idFoodMeeting));
-        User user = parameters.getUser();
+   @Override
+   public Out execute(In parameters) throws Exception {
+      Out out = new DefaultOut();
+      Connection connection = parameters.getConnection();
+      BuyerManager buyerManager = new BuyerManager(connection);
+      String idFoodMeeting = parameters.getParameter("id_food_meeting");
+      int idUser = parameters.getUser().getId();
+      List<Order> orders = getOrders(connection, idFoodMeeting);
+      Buyer buyer = buyerManager.getBuyerByFoodMeetingId(Integer.valueOf(idFoodMeeting));
+      User user = parameters.getUser();
 
-        int foodMeetingId = Integer.parseInt(parameters.getParameter("id_food_meeting"));
-        int buyerId = buyer != null ? buyer.getUserId() : 0;
+      int foodMeetingId = Integer.parseInt(parameters.getParameter("id_food_meeting"));
+      int buyerId = buyer != null ? buyer.getUserId() : 0;
 
-        String isBuyer = (buyerId == user.getId()) ? "block" : "none";
-        List<PaymentItem> itemList = getExtraItems(foodMeetingId, connection);
+      String isBuyer = (buyerId == user.getId()) ? "block" : "none";
+      List<PaymentItem> itemList = getExtraItems(foodMeetingId, connection);
 
-        double itemTotalPrice = getTotalExternalItemPrice(itemList);
-        double partialByOrder = itemTotalPrice > 0 ? itemTotalPrice / orders.size() : 0;
+      double itemTotalPrice = getTotalExternalItemPrice(itemList);
+      double partialByOrder = itemTotalPrice > 0 ? itemTotalPrice / orders.size() : 0;
 
-        FoodMeeting foodMeeting = getFoodMeeting(connection, idFoodMeeting);
-        out.addResult("foodMeeting", foodMeeting);
-        out.addResult("items", itemList);
-        out.addResult("estate", isBuyer);
-        out.addResult("id_food_meeting", foodMeetingId);
-        out.addResult("total_item_price", itemTotalPrice);
-        out.addResult("partialByOrder", partialByOrder);
-        out.addResult("myUser", user);
-        out.addResult("buyer", buyer);
-        out.addResult("orders", orders);
+      FoodMeeting foodMeeting = getFoodMeeting(connection, idFoodMeeting);
+      out.addResult("foodMeeting", foodMeeting);
+      out.addResult("items", itemList);
+      out.addResult("estate", isBuyer);
+      out.addResult("id_food_meeting", foodMeetingId);
+      out.addResult("total_item_price", itemTotalPrice);
+      out.addResult("partialByOrder", partialByOrder);
+      out.addResult("myUser", user);
+      out.addResult("buyer", buyer);
+      out.addResult("orders", orders);
 
-        if (buyer == null) {
-            return out.redirect("wheeldecide/wheel.jsp");
-        } else {
-            return out.forward("payment/payment.jsp");
-        }
-    }
+      if (buyer == null) {
+         return out.redirect("wheeldecide/wheel.jsp");
+      } else {
+         return out.forward("payment/payment.jsp");
+      }
+   }
 
-    private FoodMeeting getFoodMeeting(Connection connection, String idFoodMeeting) throws SQLException {
-        FoodMeetingManager foodMeetingManager = new FoodMeetingManager(connection);
-        return foodMeetingManager.getFoodMeetingById(Integer.parseInt(idFoodMeeting));
-    }
+   private FoodMeeting getFoodMeeting(Connection connection, String idFoodMeeting) throws SQLException {
+      FoodMeetingManager foodMeetingManager = new FoodMeetingManager(connection);
+      return foodMeetingManager.getFoodMeetingById(Integer.parseInt(idFoodMeeting));
+   }
 
-    private List<PaymentItem> getExtraItems(int foodMeetingId, Connection connection) throws Exception {
-        List<PaymentItem> resultItemsList = new ArrayList<>();
+   private List<PaymentItem> getExtraItems(int foodMeetingId, Connection connection) throws Exception {
+      List<PaymentItem> resultItemsList = new ArrayList<>();
 
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from payment where id_food_meeting=?");
-            preparedStatement.setInt(1, foodMeetingId);
-            ResultSet result = preparedStatement.executeQuery();
+      try {
+         PreparedStatement preparedStatement = connection.prepareStatement("select * from payment where id_food_meeting=?");
+         preparedStatement.setInt(1, foodMeetingId);
+         ResultSet result = preparedStatement.executeQuery();
 
-            while (result.next()) {
-                resultItemsList.add(new PaymentItem(result.getInt("id"),
-                        foodMeetingId,
-                        result.getString("item_description"),
-                        result.getDouble("price")));
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(PaymentCommand.class.getName()).log(Level.SEVERE, null, ex);
-            throw new Exception("Throw a problem when we wont work with payments table : " + ex.getMessage());
-        }
+         while (result.next()) {
+            resultItemsList.add(new PaymentItem(result.getInt("id"),
+                  foodMeetingId,
+                  result.getString("item_description"),
+                  result.getDouble("price")));
+         }
+      } catch (SQLException ex) {
+         Logger.getLogger(PaymentCommand.class.getName()).log(Level.SEVERE, null, ex);
+         throw new Exception("Throw a problem when we wont work with payments table : " + ex.getMessage());
+      }
 
-        return resultItemsList;
-    }
+      return resultItemsList;
+   }
 
-    private double getTotalExternalItemPrice(List<PaymentItem> itemList) {
-        double resp = 0;
+   private double getTotalExternalItemPrice(List<PaymentItem> itemList) {
+      double resp = 0;
 
-        for (PaymentItem paymentItem : itemList) {
-            resp += paymentItem.getPrice();
-        }
+      for (PaymentItem paymentItem : itemList) {
+         resp += paymentItem.getPrice();
+      }
 
-        return resp;
-    }
+      return resp;
+   }
 
-    private List<Order> getOrders(Connection connection, String idFoodMeeting) throws SQLException {
-        OrderManager orderManager = new OrderManager(connection);
-        return orderManager.getOrdersWithUserByFoodMeeting(Integer.parseInt(idFoodMeeting));
-    }
+   private List<Order> getOrders(Connection connection, String idFoodMeeting) throws SQLException {
+      OrderManager orderManager = new OrderManager(connection);
+      return orderManager.getOrdersWithUserByFoodMeeting(Integer.parseInt(idFoodMeeting));
+   }
 
-    private Order extractMyOrder(int idUser, List<Order> orders) {
-        Order myOrder = null;
+   private Order extractMyOrder(int idUser, List<Order> orders) {
+      Order myOrder = null;
 
-        for (Order order : orders) {
-            if (order.getIdUser() == idUser) {
-                myOrder = order;
-                break;
-            }
-        }
+      for (Order order : orders) {
+         if (order.getIdUser() == idUser) {
+            myOrder = order;
+            break;
+         }
+      }
 
-        orders.remove(myOrder);
-        return myOrder;
-    }
+      orders.remove(myOrder);
+      return myOrder;
+   }
 
-}
+}   
